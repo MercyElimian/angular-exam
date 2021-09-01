cb4f407cc949bd00dbc3da97e3fb0239765f5220
MercyElimian <82936118+MercyElimian@users.noreply.github.com>
2021-09-01 21:16:49 +0100

    Add files via upload

---
+++ exam.js
@@ -0,0 +1,28 @@
+//Number 1
+String
+
+
+//Number 2
+Boolean
+
+
+//Number 3
+var oddNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].filter
+((number) => number%2!==0);
+console.log(oddNumbers);
+
+
+
+//Number 4
+var student = {
+  name : "John Doe", 
+  age : 30, 
+  gender : "Male",
+  nationality : "Korean",
+  numberOfLanguages : 3, 
+  favouriteMusic : ["I shot the Sheriff", "Kokonbilo", "It’s a man’s world"] 
+ }
+
+
+//Number 5
+module
